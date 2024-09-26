86fdea05d47740a4b4df7ba1d78626af4392a648
Abdulmuiz180 <abdlmuiz180@gmail.com>
2024-09-26 21:39:12 +0700

    Add files via upload

---
+++ Tugas mandiri_1.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+	
+	printf("Nama      : ABDUL MUIZ.\n");
+	
+	printf("Prodi     : TEKNIK INFORMATIKA.\n");
+	
+	printf("NIM       : 2441050.\n");
+	
+	return 0;
+}
